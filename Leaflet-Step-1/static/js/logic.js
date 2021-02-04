c3bbd9fba345061ad7ac7757857f87f266e8341a
Juan Ramirez <juanramirez@Juans-MacBook-Air.local>
2021-02-03 22:08:33 -0600

    js logic updates

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,97 +1,93 @@
-// go to he website 
-let endpoint = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2021-01-18&endtime=2021-01-25"
 
-// starting point for the map
+// Map object with starting position
 let myMap = L.map('map').setView([37.76230904443554, -122.43193564741273], 4.5);
 
-// map properties
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
+  // Add mapbox layer
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
+    maxZoom: 18,
     zoomOffset: -1,
-    id: 'mapbox/light-v10',
-    accessToken: API_KEY,
-    title: "Earthquakes Mapping"
-}).addTo(myMap);
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
 
-// the array of colors and limits to use in the circles and in the label
-let colors = ["#7FFF00","#ADFF2F","#FFFF00","#FFD700","#FFA500","#FF4500"]
-let limits = ["0-1","1-2","2-3","3-4","4-5","5+"]
-
-/**
- * Function that receives a feature from GeoJSON and according to magnitude decides
- * which color to return for the circles
- * @param {Object} feature from GeoJSON 
- * @param {Object} layer from Leaflet
- * @returns {String} containing the fillColor for the point
- */
-function fillColor(feature,layer){
-    // transform the property into a number
-    let mag = +feature.properties.mag;
-    // select the color depending on the magnitude
-    if(mag > 0 && mag < 1){
-        return colors[0]
-    }else if(mag < 2){
-        return colors[1]
-    }else if(mag < 3){
-        return colors[2]
-    }else if(mag < 4){
-        return colors[3]
-    }else if(mag < 5){
-        return colors[4]
-    }else {
-        return colors[5]
+//Set color for magnitudes
+function getColor(feature){
+    let color=""
+    let magnitude=feature.properties.mag
+    if(magnitude>5){
+        color="#FF4500"
+    } else if(magnitude>4){
+        color="#FFA500"
+    } else if(magnitude>3){
+        color="#FFD700"
+    } else if(magnitude>2){
+        color="#FFFF00"
+    } else if(magnitude>1){
+        color="#ADFF2F"
+    } else {
+        color="#7FFF00"
     }
+    return color
+}
+// Create function for marker size
+let markers=[]
+function markerSize(mag){
+    return mag*15000
+}
+// Get coordinates
+function getcoord(coordinates){
+    array=[coordinates[1],coordinates[0]]
+    return array
 }
 
-
-// Read the endpoint to get the data from the USGS
-d3.json(endpoint).then(data => {
-    console.log(data)
-    
-    let myCircleStyle = {
-        color: "black",
-        weight: 1,
-        opacity: 0.5,
-        fillColor: "#088",
-        fillOpacity: 0.75,
-        radius: 8
-    };
-    
-    // Add points to my map and change properties depending on properties of the feature
-    let geojson = L.geoJson(data,{
-        pointToLayer: function(data, latlng){
-            myCircleStyle.radius = data.properties.mag*10;
-            myCircleStyle.fillColor = fillColor(data,latlng)
-            return L.circleMarker(latlng, myCircleStyle)
-        },
-        onEachFeature: onEachFeature //this calls the function to bind Popups on each feature
-    }).addTo(myMap)
-    
-    // Add markers to my map
-    // markers.addTo(myMap)
-
-    // Set up the legend
-    let legend = L.control({ 
-        position: "bottomright"
-     });
-
-    legend.onAdd = function() {
-        let div = L.DomUtil.create("div", "info legend");
-        let labels = [];
-
-
-        // add the scale of colors with the values considered inside
-        limits.forEach(function(limit, index) {
-            labels.push("<li style=\"background-color: " + colors[index] + "\">"+limit+"</li>");
-        });
-
-        div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-        return div;
-    };
-
-    // Adding legend to the map
-    legend.addTo(myMap);
-
+// get data from Geojson
+url="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+d3.json(url).then(data=>{
+    data["features"].forEach(d=>{
+         L.circle(getcoord(d.geometry.coordinates),{
+            stroke:false,
+            fillOpacity:.75,
+            color:getColor(d),
+            fillColor:getColor(d),
+            radius:markerSize(d.properties.mag)
+            }).bindPopup(`<h3>Type: ${d.properties.type}</h3> <hr> <h3>Location: ${d.properties.place}</h3> <hr> <h3>Mag: ${d.properties.mag}</h3>`).addTo(myMap)
+    })
 })
+.catch(e=>{
+    console.log(e)
+})
+
+// Set up the legend
+let legend = L.control({ 
+    position: "bottomright"
+ });
+ let colors=["#7FFF00","#ADFF2F","#FFFF00","#FFD700","#FFA500","#FF4500"]
+ let limits=["0-1","1-2","2-3","3-4","4-5","5+"]
+ 
+legend.onAdd = function() {
+    let div = L.DomUtil.create("div", "info legend");
+    let labels = [];
+
+    // Add min & max
+    let legendInfo = "<p>Magnitudes</p>" +
+    "<div class=\"labels\">" +
+        "<div class=\"min\">" + limits[0] + "</div>" +
+        "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+    "</div>";
+
+    div.innerHTML = legendInfo;
+
+    // add the scale of colors with the values considered inside
+    limits.forEach(function(limit, index) {
+        labels.push("<li style=\"background-color: " + colors[index] + "\">"+limit+"</li>");
+    });
+
+    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+    return div;
+};
+
+// Adding legend to the map
+legend.addTo(myMap);
+
